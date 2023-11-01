a369eeddf019d044ba63c4d34ffb8c44842bb6e2
Naman Saini <saininaman1103@gmail.com>
2023-11-01 20:00:56 +0530

    Busy Man SPOJ Contest Problem in java

---
+++ Bit_Manipulation/BusyMan_SPOJ.java
@@ -0,0 +1,19 @@
+package Bit_Manipulation;
+
+import java.util.Scanner;
+
+public class BusyMan_SPOJ
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            
+        }
+
+    }
+
+
+}
